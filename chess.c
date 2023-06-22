8ad3f67a760307a63a177a68c56e90d9cb49d814
osmanhayek <ohayek@DESKTOP-ETK3OP3>
2023-06-23 00:19:45 +0300

    added a new struct and is_check_mate function (not finished yet)

--- chess.c
+++ chess.c
@@ -104,3 +104,106 @@ int	ft_strlen(char *str)
 		i++;
 	return (i);
 }
+
+int **can_attack_collection(chess_piece *pieces, int num_of_pieces, int *new_size)
+{
+	int	**result;
+	int	size;
+	int	index;
+
+	size = 0;
+	index = 0;
+	for (int i = 0; i < num_of_pieces; i++)
+		size += pieces[i].num_can_attack;
+	result = (int **)malloc(sizeof(int *) * size);
+	for (int i = 0; i < num_of_pieces; i++)
+	{
+		for (int j = 0; j < pieces[i].num_can_attack; j++)
+		{
+			result[index] = (int *)malloc(sizeof(int) * 2);
+			result[index][0] = pieces[i].can_attack[j][0];
+			result[index++][1] = pieces[i].can_attack[j][1];
+		}
+	}
+	*new_size = index;
+	return (result);
+}
+
+int in_the_collection(int size,int **coll, int x, int y)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (x == coll[i][0] && y == coll[i][1])
+			return (1);
+	}
+	return (0);
+}
+
+int is_check_mate(chess_piece *pieces, int num_of_pieces ,int board_size, int x_k, int y_k)
+{
+	int **collection;
+	int	size;
+	
+	collection = can_attack_collection(pieces, num_of_pieces,&size);
+	//check if the king is in check 
+	if (in_the_collection(size, collection, x_k, y_k))
+	{
+		if (!in_the_collection(size, collection, x_k + 1, y_k))
+			return (0);
+		if (!in_the_collection(size, collection, x_k, y_k + 1))
+			return (0);
+		if (!in_the_collection(size, collection, x_k - 1, y_k))
+			return (0);
+		if (!in_the_collection(size, collection, x_k, y_k - 1))
+			return (0);
+		if (!in_the_collection(size, collection, x_k - 1, y_k - 1))
+			return (0);
+		if (!in_the_collection(size, collection, x_k - 1, y_k + 1))
+			return (0);
+		if (!in_the_collection(size, collection, x_k + 1, y_k + 1))
+			return (0);
+		if (!in_the_collection(size, collection, x_k + 1, y_k - 1))
+			return (0);
+		return (1);
+	}
+	return (0);
+}
+
+void    set_attack_for_rock(chess_piece *rock, int **coor, int coor_size, int size)
+{
+	int	i;
+	int index;
+
+	rock->can_attack = (int **)malloc(sizeof(int *) * (size * size));
+	index = 0;
+	for (i = rock->x; i < size; i++)
+	{
+		if (in_coor(i, rock->y, coor, coor_size) && (i != rock->x))
+			break;
+		rock->can_attack[index] = (int *)malloc(sizeof(int) * 2);
+		rock->can_attack[index][0] = i; rock->can_attack[index++][1] = rock->y;
+	}
+	for (i = rock->x; i >= 0; i--)
+	{
+		if (in_coor(i, rock->y, coor, coor_size) && (i != rock->x))
+			break;
+		rock->can_attack[index] = (int *)malloc(sizeof(int) * 2);
+		rock->can_attack[index][0] = i; rock->can_attack[index++][1] = rock->y;
+	}
+	for (i = rock->y; i < size; i++)
+	{
+		if (in_coor(rock->x, i, coor, coor_size) && (i != rock->y))
+			break;
+		rock->can_attack[index] = (int *)malloc(sizeof(int) * 2);
+		rock->can_attack[index][0] = rock->x ;rock->can_attack[index++][1] = i;
+	}
+	for (i = rock->y; i >= 0; i--)
+	{
+		if (in_coor(rock->x, i, coor, coor_size) && (i != rock->y))
+			break;
+		rock->can_attack[index] = (int *)malloc(sizeof(int) * 2);
+		rock->can_attack[index][0] = rock->x ;rock->can_attack[index++][1] = i;
+	}
+	rock->num_can_attack = index;
+	return (0);
+}
--- chess.h
+++ chess.h
@@ -3,11 +3,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int	check_rock(int size, int x, int y, int **coor, int coor_size, int x_k, int y_k);
-int	check_bichop(int size, int x, int y, int **coor, int coor_size, int x_k, int y_k);
-int	check_pawn(int size, int x, int y, int x_k, int y_k);
-int	check_queen(int size, int x, int y, int **coor, int coor_size, int x_k, int y_k);
-int	in_coor(int x, int y, int **coor, int size);
-int	ft_strlen(char *str);
+typedef struct s_chess_piece
+{
+    char    type;
+    int     x;
+    int     y;
+    int     num_can_attack;
+    int     **can_attack;
+}chess_piece;
+
+int	    check_rock(int size, int x, int y, int **coor, int coor_size, int x_k, int y_k);
+int	    check_bichop(int size, int x, int y, int **coor, int coor_size, int x_k, int y_k);
+int	    check_pawn(int size, int x, int y, int x_k, int y_k);
+int	    check_queen(int size, int x, int y, int **coor, int coor_size, int x_k, int y_k);
+int	    in_coor(int x, int y, int **coor, int size);
+int	    ft_strlen(char *str);
+void    set_attack_for_rock(chess_piece rock, int **coor, int coor_size);
+int     in_the_collection(int size,int **coll, int x, int y);
+int     is_check_mate(chess_piece *pieces, int board_size, int num_of_pieces ,int x_k, int y_k);
+int     **can_attack_collection(chess_piece *pieces, int num_of_pieces, int new_size);
 
 #endif
